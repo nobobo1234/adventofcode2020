4f053b8babc0198504e1762487134f0effce2cb8
nobobo1234 <noahvanboven@gmail.com>
2020-12-01 16:00:54 +0100

    Adding day 1 solution

--- day1/day1.js
+++ day1/day1.js
@@ -1,14 +1,36 @@
 // Advent of Code day 1
 const fs = require('fs');
 const path = require('path');
-const input = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8');
+const input = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
+    .split('\n')
+    .map(Number);
 
 function part1() {
-    return;
+    let result = 0;
+    for (const expense of input) {
+        for (const expense2 of input) {
+            if (expense + expense2 === 2020) {
+                result = expense * expense2;
+                break;
+            }
+        }
+    }
+    return result;
 }
 
 function part2() {
-    return;
+    let result = 0;
+    for (const expense of input) {
+        for (const expense2 of input) {
+            for (const expense3 of input) {
+                if (expense + expense2 + expense3 === 2020) {
+                    result = expense * expense2 * expense3;
+                    break;
+                }
+            }
+        }
+    }
+    return result;
 }
 
 console.log('The answer to day 1 part 1 is:', part1());
