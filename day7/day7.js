fdf72144f9818780e6d860751213af11e01c4f78
nobobo1234 <noahvanboven@gmail.com>
2020-12-07 13:48:25 +0100

    Day 7 WIP

--- day7/day7.js
+++ day7/day7.js
@@ -8,12 +8,12 @@ const input = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
     })
     .filter(e => !e.includes(''));
 
-
 class Graph {
     constructor() {
         this.edges = new Map();
         this.nodes = [];
         this.outerEdges = new Set();
+        this.total = 0;
     }
 
     addNode(name) {
@@ -21,14 +21,16 @@ class Graph {
         this.edges.set(name, []);
     }
 
-    addEdge(node1, node2) {
-        this.edges.get(node2).push(node1);
+    addEdge(node1, node2, number) {
+        this.edges.get(node2).push([node1, number]);
     }
 
     traverseUp(node) {
         if (this.edges.get(node).length) {
             for (const childNode of this.edges.get(node)) {
-                this.traverseUp(childNode);
+                this.outerEdges.add(node);
+                this.total += childNode[1];
+                this.traverseUp(childNode[0]);
             }
         } else {
             this.outerEdges.add(node);
@@ -44,21 +46,24 @@ function part1() {
             graph.addNode(rule[0]);
         }
         const contains = rule[1].split(', ')
-            .map(e => e.slice(2))
             .map(e => e.endsWith('s') ? e.slice(0, -1) : e);
         for (const bag of contains) {
+            const bagName = bag.slice(2);
+            const bagNumber = Number(bag.slice(0, 1));
             if (bag === ' other bag') {
                 continue;
             }
             if (!graph.edges.has(bag)) {
                 graph.addNode(bag);
+                console.log(graph.edges.get(bagName))
+
             }
-            graph.addEdge(rule[0], bag);
+            graph.addEdge(rule[0], bagName, bagNumber);
         }
     }
     graph.traverseUp('shiny gold bag');
 
-    return graph.outerEdges.size;
+    return graph.outerEdges.size - 1; // Also counts shiny gold, so do not count it
 }
 
 function part2() {
